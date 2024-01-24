2c877338612ca23a14aa1724b9d88c019a1015d0
farhullin2017@gmail.com <90451035+IlnazFarkhullin@users.noreply.github.com>
2024-01-24 17:15:01 +0300

    ArrayList

---
+++ ArrayList_1/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections; //для работы обязтельно подключаем это пространство имён!!!
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArrayList_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            ArrayList array = new ArrayList()//создаём коллекцию
+            {
+               228,
+               223,
+               "Ильназ Алмазович",
+               "Ваня",
+               '%',
+               22.8
+            };
+            Console.WriteLine("Выводим коллекцию:");
+            foreach (var i in array)
+            {
+                Console.WriteLine(i);
+            }
+            Console.WriteLine();
+
+            //Добавление 
+            Console.WriteLine("Добавление:");
+            array.Add("ОАиП");
+            array.Add("БД");
+            array.Add(5);
+            array.Add('!');
+            //также можно добавить массив
+            array.AddRange(new string[] { "Автоматов", "не", "будет", "!"});
+            foreach (var i in array)
+            {
+                Console.Write(i + " ");
+            }
+            Console.WriteLine();
+
+            //Удаление
+            Console.WriteLine("Удаление:");
+            array.Remove("ОАиП");//удаляем по значению
+            array.RemoveAt(3); //удаляем по индексу
+            foreach (var i in array)
+            { 
+                Console.Write(i + " ");
+            }
+            Console.WriteLine();
+
+            Console.WriteLine("Переворачиваем коллекцию:");
+            array.Reverse();
+            foreach (var i in array)
+            {
+                Console.Write(i + " ");
+            }
+            Console.WriteLine();
+            Console.WriteLine("Получаем элеент по индексу:");
+            Console.WriteLine(array[9]);
+            Console.ReadKey();
+        }
+    }
+}
---
+++ ArrayList_2/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArrayList_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Дан список с именами друзей Ильназа Алмазовича. Добавить ещё три имени которые пользователь вводит с клавиатуры и выполнить поиск по имени, затем удалить найденное имя
+            ArrayList s = new ArrayList();
+            s.Add("Сева");
+            s.Add("Ильяс");
+            s.Add("Макс");
+            s.Add("Марсель");
+
+
+            Console.WriteLine("Добавляем имена:");
+            for (int i = 0; i<3; i++)
+            {
+                string fruct = Console.ReadLine();
+                s.Add (fruct);
+            }
+            
+            foreach (var i in s )
+            {
+                Console.WriteLine(i);
+            }
+            Console.WriteLine();
+            Console.WriteLine("Поиск: ");
+            string del = Console.ReadLine();
+
+            if (s.Contains(del))
+            {
+                s.Remove(del);
+                Console.WriteLine($"{del} удалён из друзей Ильназа Алмазовича");
+            }
+            else
+            {
+                Console.WriteLine($"такого человека в коллекции нет");
+            }
+            Console.ReadKey();
+        }
+    }
+}
